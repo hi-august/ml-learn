5d92a506ac7073ce5e0fdfbb74e68ef171ca52cf
augustles <1927064778@qq.com>
2017-06-22 15:00:19 +0800

    u

--- tensorflow/simple_comment_sort.py
+++ tensorflow/simple_comment_sort.py
@@ -62,7 +62,7 @@ def normalize_dataset(lex):
     dataset = []
     # lex:词汇表；review:评论；clf:评论对应的分类，[0,1]代表负面评论 [1,0]代表正面评论
     def string_to_vector(lex, review, clf):
-        words = word_tokenize(line.lower())
+        words = word_tokenize(line.lower().decode('utf-8'))
         lemmatizer = WordNetLemmatizer()
         words = [lemmatizer.lemmatize(word) for word in words]
 
@@ -120,7 +120,7 @@ def neural_network(data):
     # 定义输出层"神经元"的权重和biases
     layer_output_w_b = {'w_':tf.Variable(tf.random_normal([n_layer_2, n_output_layer])), 'b_':tf.Variable(tf.random_normal([n_output_layer]))}
 
-    # w·x+b
+    # w.x+b
     layer_1 = tf.add(tf.matmul(data, layer_1_w_b['w_']), layer_1_w_b['b_'])
     layer_1 = tf.nn.relu(layer_1)  # 激活函数
     layer_2 = tf.add(tf.matmul(layer_1, layer_2_w_b['w_']), layer_2_w_b['b_'])
@@ -138,19 +138,19 @@ Y = tf.placeholder('float')
 # 使用数据训练神经网络
 def train_neural_network(X, Y):
     predict = neural_network(X)
-    cost_func = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(predict, Y))
+    cost_func = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predict, labels=Y))
     optimizer = tf.train.AdamOptimizer().minimize(cost_func)  # learning rate 默认 0.001
 
     epochs = 13
     with tf.Session() as session:
         session.run(tf.initialize_all_variables())
-        epoch_loss = 0
-
-        i = 0
         random.shuffle(train_dataset)
         train_x = dataset[:, 0]
         train_y = dataset[:, 1]
         for epoch in range(epochs):
+            # 每次epoch应该重置i和epoch_loss为0，要不然其实只训练一个epoch
+            epoch_loss = 0
+            i = 0
             while i < len(train_x):
                 start = i
                 end = i + batch_size
@@ -168,6 +168,6 @@ def train_neural_network(X, Y):
         text_y = test_dataset[:, 1]
         correct = tf.equal(tf.argmax(predict,1), tf.argmax(Y,1))
         accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-        print('准确率: ', accuracy.eval({X:list(text_x) , Y:list(text_y)}))
+        print(u'准确率: ', accuracy.eval({X:list(text_x) , Y:list(text_y)}))
 
 train_neural_network(X,Y)
